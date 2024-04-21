e7a9bd12479df7a13884e31dc6323eef12775611
Charlize <148724296+charlilize@users.noreply.github.com>
2024-04-21 03:22:36 -0700

    test

--- src/client.js
+++ src/client.js
@@ -4,4 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 const URL = process.env.REACT_APP_SUPABASE_URL; // variables specified to nellify
 const API_KEY = process.env.REACT_APP_SUPABASE_API_KEY;
 
+console.log(URL);
+console.log(API_KEY);
+
 export const supabase = createClient(URL, API_KEY);
