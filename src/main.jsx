b378d177be8e31816ae93c393d00da28847434e8
Charlize <148724296+charlilize@users.noreply.github.com>
2024-04-20 17:26:57 -0700

    add nav bar

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from 'routes/Layout.jsx'
-import Forum from 'routes/Forum.jsx'
+import Layout from './routes/Layout.jsx'
+import Forum from './routes/Forum.jsx'
 import PostDetails from './site-components/PostDetails.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}/>
-          <Route index={true} path="/" element={<App/>}>
-          <Route index={false} path="/forum" element={<Forum/>}/>
-          <Route index={false} path="/postDetails/:postid" element={<PostDetails/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index={true} path="/" element={<App/>}/>
+            <Route index={false} path="/forum" element={<Forum/>}/>
+            <Route index={false} path="/postDetails/:postid" element={<PostDetails/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>  
   </React.StrictMode>,
 )
---
+++ src/routes/Layout.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+import React from "react";
+
+// Contains the navigation bar of the site
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul className="flex justify-between ">
+          <li className="text-xl">NoDilemma</li>
+          <div className="flex gap-7">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/forum">Forum</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
