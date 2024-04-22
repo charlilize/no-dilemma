248df0e0e39a0b12494312d36329143f272665c8
Charlize <148724296+charlilize@users.noreply.github.com>
2024-04-22 00:47:47 -0700

    fix

--- src/routes/Forum.jsx
+++ src/routes/Forum.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Input } from "@/components/ui/search-input"
 import { Button } from "@/components/ui/button"
 import Post from "../site-components/Post"
